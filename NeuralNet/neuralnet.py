cf4eea78b9830e3f02189e378c189a19a0ba87e9
benilak <bregan1992@gmail.com>
2018-03-14 22:30:57 -0600

    Adding k-nearest neighbors script (02 Prove)

--- NeuralNet/neuralnet.py
+++ NeuralNet/neuralnet.py
@@ -233,3 +233,23 @@ class NetClassifier:
             cycle_correct.append(ncorrect)
 
         return self.layers, cycle_correct
+
+pima = pd.read_csv('pima-indians-diabetes.txt', header=None)
+pima_data, pima_targets = np.hsplit(pima, [8])
+pima_data = pd.DataFrame(normalize(pima_data))
+pima_targets = pima_targets.iloc[:, 0]
+data_train, data_test, targets_train, targets_test = train_test_split(pima_data, pima_targets, train_size=0.8)
+
+classifier = NetClassifier(data_train, targets_train, [6])
+neuralnet = classifier.build_net(cycles=200)
+cycle_correct = pd.Series(neuralnet[1])
+plot = cycle_correct.plot.line()
+plt.show()
+model = NetModel(neuralnet[0])
+predictions = model.predict(data_test, targets_test)
+print(predictions[1])
+
+# classifier = MLPClassifier((6))
+# classifier.fit(data_train, targets_train)
+# predictions = classifier.predict(data_test)
+# print(check_accuracy(predictions, targets_test))
